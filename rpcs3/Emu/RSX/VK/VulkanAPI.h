7edbb60b1322fc2646769b2d54fcdd41e64f9b4b
kd-11 <karokidii@gmail.com>
2016-03-06 23:24:00 +0300

    Define WIN32_KHR only for windows

--- rpcs3/Emu/RSX/VK/VulkanAPI.h
+++ rpcs3/Emu/RSX/VK/VulkanAPI.h
@@ -1,6 +1,11 @@
 #pragma once
 
+#ifdef _WIN32
 #define VK_USE_PLATFORM_WIN32_KHR
+#else
+#define VK_USE_PLATFORM_XLIB_KHR
+#endif
+
 #include <vulkan/vulkan.h>
 #include <vulkan/vk_sdk_platform.h>
 #include "Utilities/types.h"

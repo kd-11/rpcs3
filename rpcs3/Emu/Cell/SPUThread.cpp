a99ac4d9d850f1a1cce6a54592e68588a101d5f3
kd-11 <karokidii@gmail.com>
2022-04-20 17:42:56 +0300

    spu: Naively drop writer-lock

--- rpcs3/Emu/Cell/SPUThread.cpp
+++ rpcs3/Emu/Cell/SPUThread.cpp
@@ -2734,7 +2734,7 @@ bool spu_thread::do_putllc(const spu_mfc_cmd& args)
 		{
 			// Full lock (heavyweight)
 			// TODO: vm::check_addr
-			vm::writer_lock lock(addr);
+			//vm::writer_lock lock(addr);
 
 			if (cmp_rdata(rdata, super_data))
 			{
@@ -2907,7 +2907,7 @@ void do_cell_atomic_128_store(u32 addr, const void* to_write)
 		{
 			// Full lock (heavyweight)
 			// TODO: vm::check_addr
-			vm::writer_lock lock(addr);
+			//vm::writer_lock lock(addr);
 			mov_rdata(super_data, *static_cast<const spu_rdata_t*>(to_write));
 			res += 64;
 		}

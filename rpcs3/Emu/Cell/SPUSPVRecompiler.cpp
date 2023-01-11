ba2b4f1c6c1c2753d5e868e3a18904d1c92d593e
kd-11 <karokidii@gmail.com>
2023-01-11 03:43:40 +0300

    Initial commit

---
+++ rpcs3/Emu/Cell/SPUSPVRecompiler.cpp
@@ -0,0 +1,802 @@
+#include "SPUSPVRecompiler.h"
+
+void spv_recompiler::UNK(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::STOP(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::LNOP(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::SYNC(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::DSYNC(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::MFSPR(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::RDCH(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::RCHCNT(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::SF(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::OR(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::BG(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::SFH(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::NOR(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::ABSDB(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::ROT(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::ROTM(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::ROTMA(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::SHL(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::ROTH(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::ROTHM(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::ROTMAH(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::SHLH(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::ROTI(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::ROTMI(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::ROTMAI(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::SHLI(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::ROTHI(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::ROTHMI(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::ROTMAHI(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::SHLHI(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::A(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::AND(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::CG(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::AH(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::NAND(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::AVGB(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::MTSPR(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::WRCH(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::BIZ(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::BINZ(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::BIHZ(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::BIHNZ(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::STOPD(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::STQX(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::BI(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::BISL(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::IRET(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::BISLED(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::HBR(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::GB(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::GBH(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::GBB(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::FSM(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::FSMH(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::FSMB(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::FREST(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::FRSQEST(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::LQX(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::ROTQBYBI(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::ROTQMBYBI(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::SHLQBYBI(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::CBX(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::CHX(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::CWX(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::CDX(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::ROTQBI(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::ROTQMBI(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::SHLQBI(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::ROTQBY(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::ROTQMBY(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::SHLQBY(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::ORX(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::CBD(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::CHD(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::CWD(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::CDD(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::ROTQBII(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::ROTQMBII(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::SHLQBII(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::ROTQBYI(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::ROTQMBYI(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::SHLQBYI(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::NOP(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::CGT(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::XOR(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::CGTH(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::EQV(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::CGTB(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::SUMB(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::HGT(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::CLZ(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::XSWD(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::XSHW(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::CNTB(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::XSBH(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::CLGT(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::ANDC(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::FCGT(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::DFCGT(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::FA(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::FS(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::FM(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::CLGTH(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::ORC(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::FCMGT(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::DFCMGT(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::DFA(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::DFS(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::DFM(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::CLGTB(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::HLGT(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::DFMA(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::DFMS(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::DFNMS(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::DFNMA(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::CEQ(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::MPYHHU(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::ADDX(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::SFX(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::CGX(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::BGX(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::MPYHHA(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::MPYHHAU(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::FSCRRD(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::FESD(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::FRDS(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::FSCRWR(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::DFTSV(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::FCEQ(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::DFCEQ(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::MPY(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::MPYH(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::MPYHH(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::MPYS(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::CEQH(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::FCMEQ(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::DFCMEQ(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::MPYU(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::CEQB(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::FI(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::HEQ(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::CFLTS(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::CFLTU(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::CSFLT(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::CUFLT(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::BRZ(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::STQA(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::BRNZ(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::BRHZ(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::BRHNZ(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::STQR(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::BRA(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::LQA(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::BRASL(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::BR(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::FSMBI(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::BRSL(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::LQR(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::IL(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::ILHU(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::ILH(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::IOHL(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::ORI(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::ORHI(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::ORBI(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::SFI(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::SFHI(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::ANDI(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::ANDHI(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::ANDBI(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::AI(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::AHI(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::STQD(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::LQD(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::XORI(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::XORHI(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::XORBI(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::CGTI(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::CGTHI(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::CGTBI(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::HGTI(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::CLGTI(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::CLGTHI(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::CLGTBI(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::HLGTI(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::MPYI(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::MPYUI(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::CEQI(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::CEQHI(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::CEQBI(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::HEQI(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::HBRA(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::HBRR(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::ILA(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::SELB(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::SHUFB(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::MPYA(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::FNMS(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::FMA(spu_opcode_t op)
+{
+}
+
+void spv_recompiler::FMS(spu_opcode_t op)
+{
+}
+

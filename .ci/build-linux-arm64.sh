2262dc53233e417dfb8585a2ff77a433b91447c1
kd-11 <karokidii@gmail.com>
2024-09-17 03:09:04 +0300

    Remove sdl2 version specificity

--- .ci/build-linux-arm64.sh
+++ .ci/build-linux-arm64.sh
@@ -5,7 +5,7 @@ if [ -z "$CIRRUS_CI" ]; then
 fi
 
 apt -y update && \
-apt -y install clang build-essential libasound2-dev libpulse-dev libopenal-dev libglew-dev zlib1g-dev libedit-dev libvulkan-dev libudev-dev git libevdev-dev libsdl2-2.0 libsdl2-dev libjack-dev libsndio-dev
+apt -y install clang build-essential libasound2-dev libpulse-dev libopenal-dev libglew-dev zlib1g-dev libedit-dev libvulkan-dev libudev-dev git libevdev-dev libsdl2-dev libjack-dev libsndio-dev
 apt -y install qt6-base-dev qt6-declarative-dev qt6-multimedia-dev qt6-svg-dev
 
 sh -ex .ci/build-linux.sh

f694000454305ea94af0bce4c0beb6b359bb1715
kd-11 <karokidii@gmail.com>
2025-12-25 23:56:54 +0300

    rsx: Fix shader interpreter variants generation

--- rpcs3/Emu/RSX/Program/ShaderInterpreter.cpp
+++ rpcs3/Emu/RSX/Program/ShaderInterpreter.cpp
@@ -5,54 +5,67 @@
 
 namespace program_common::interpreter
 {
-    std::vector<interpreter_variant_t> get_interpreter_variants()
-    {
-        // Separable passes to fetch all possible variants
-        std::unordered_set<u32> fs_masks;
-        for (u32 fs_opt = COMPILER_OPT_FS_MIN, fs_opt_bit = fs_opt;
-            fs_opt <= COMPILER_OPT_FS_MAX; fs_opt++, fs_opt_bit <<= 1)
-        {
-            if (fs_opt_bit & COMPILER_OPT_ALPHA_TEST_MASK)
-            {
-                continue;
-            }
+	void bitrange_foreach(u32 min, u32 max, std::function<void(u32)> func)
+	{
+		if (max <= min)
+		{
+			return;
+		}
 
-            fs_masks.insert(fs_opt);
-        }
+		const u32 shift = std::countr_zero(min);
+		const u32 a = min >> shift;
+		const u32 b = (max + max) >> shift;
 
-        // Now we add in the alpha testing variants for all fs variants.
-        // Only one alpha test type is usable at once
-        std::unordered_set<u32> fs_alpha_test_masks;
-        for (u32 alpha_test_bit = COMPILER_OPT_ENABLE_ALPHA_TEST_GE;
-            alpha_test_bit <= COMPILER_OPT_ENABLE_ALPHA_TEST_NE;
-            alpha_test_bit <<= 1)
-        {
-            for (const auto& mask : fs_masks)
-            {
-                fs_alpha_test_masks.insert(mask | alpha_test_bit);
-            }
-        }
+		for (u32 acc = a; acc < b; acc++)
+		{
+			func(acc << shift);
+		}
+	}
 
-        // VS
-        std::unordered_set<u32> vs_masks;
-        for (u32 vs_opt = COMPILER_OPT_VS_MIN; vs_opt <= COMPILER_OPT_VS_MAX; ++vs_opt)
-        {
-            vs_masks.insert(vs_opt);
-        }
+	std::vector<interpreter_variant_t> get_interpreter_variants()
+	{
+		// Separable passes to fetch all possible variants
+		std::unordered_set<u32> fs_masks;
+		fs_masks.insert(0);
+		bitrange_foreach(COMPILER_OPT_FS_MIN, COMPILER_OPT_FS_MAX, [&](u32 fs_opt)
+		{
+			fs_masks.insert(fs_opt);
+		});
 
-        // Merge all FS variants
-        fs_masks.merge(fs_alpha_test_masks);
+		// Now we add in the alpha testing variants for all fs variants.
+		// Only one alpha test type is usable at once
+		std::unordered_set<u32> fs_alpha_test_masks;
+		for (u32 alpha_test_bit = COMPILER_OPT_ENABLE_ALPHA_TEST_GE;
+			alpha_test_bit <= COMPILER_OPT_ENABLE_ALPHA_TEST_NE;
+			alpha_test_bit <<= 1)
+		{
+			for (const auto& mask : fs_masks)
+			{
+				fs_alpha_test_masks.insert(mask | alpha_test_bit);
+			}
+		}
 
-        // Prepare outputs
-        std::vector<interpreter_variant_t> results;
-        for (const auto& vs_opt : vs_masks)
-        {
-            for (const auto& fs_opt : fs_masks)
-            {
-                results.push_back({ vs_opt, fs_opt });
-            }
-        }
+		// VS
+		std::unordered_set<u32> vs_masks;
+		vs_masks.insert(0);
+		bitrange_foreach(COMPILER_OPT_VS_MIN, COMPILER_OPT_VS_MAX, [&](u32 vs_opt)
+		{
+			vs_masks.insert(vs_opt);
+		});
 
-        return results;
-    }
+		// Merge all FS variants
+		fs_masks.merge(fs_alpha_test_masks);
+
+		// Prepare outputs
+		std::vector<interpreter_variant_t> results;
+		for (const auto& vs_opt : vs_masks)
+		{
+			for (const auto& fs_opt : fs_masks)
+			{
+				results.push_back({ vs_opt, fs_opt });
+			}
+		}
+
+		return results;
+	}
 }
